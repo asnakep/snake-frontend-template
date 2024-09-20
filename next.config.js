a59151823a44bdfbcbd7853658c7a07f5e818207
Manuel <econcotelli@proton.me>
2024-09-20 14:04:17 +0000

    updated below files

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   output: 'standalone',
   reactStrictMode: true,
   webpack: (config) => {
     config.experiments = {
       asyncWebAssembly: true,
       topLevelAwait: true,
-      layers: true
-    }
+      layers: true,
+    };
 
-   // Set Webpack output environment configuration for async/await support
+    // Set Webpack output environment configuration for async/await support
     config.output.environment = {
       ...config.output.environment,
       asyncFunction: true,
     };
 
-    return config
-  }
-}
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.koios.rest/api/v1/:path*',
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 "use client";
 import dynamic from "next/dynamic";
 const Stake = dynamic(() => import("./Delegate"), { ssr: false });
+const Koios = dynamic(() => import("./Koios"), { ssr: false });
 
 export default function Main() {
   return (
     <div className="absolute top-18 right-20">
       <Stake />
+      <Koios />
     </div>
   );
 }
