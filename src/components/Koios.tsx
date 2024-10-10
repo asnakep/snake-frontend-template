4559c42390bcd9a8f670bb182fe6e82175c99606
Manuel <econcotelli@proton.me>
2024-10-10 18:05:29 +0000

    mods

--- src/components/Koios.tsx
+++ src/components/Koios.tsx
@@ -31,7 +31,7 @@ const Koios = () => {
       ) : (
         <div>
           <p>{poolStats?.name || ''}</p>
-          <p>Ticker: {poolStats?.ticker || ''}</p>
+          <p>Delegation Ticker: {poolStats?.ticker || ''}</p>
           <p>Pool ID: {poolStats?.poolIDBech || ''}</p>
           <p>Delegators: {poolStats?.liveDelegators}</p>
           <p>Epoch Cost: {poolStats?.fixedCost}</p>
