226fc67f56430aaabedd877c74587aeac8b19dcf
Manuel <econcotelli@proton.me>
2024-10-11 19:01:23 +0000

    mod

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -25,7 +25,7 @@ const componentsConfig = [
   {
     id: "epochschedules",
     component: EpochSchedules,
-    position: { top: "120px", left: "50px", position: "fixed" },
+    position: { top: "120px", center: "30px", position: "fixed" },
   },
 ];
 
