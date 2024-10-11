a67c00180b06fd29c6dfe47fc220b71add28a589
Manuel <econcotelli@proton.me>
2024-10-11 12:33:11 +0000

    mod

--- src/components/queries/topHeaders.ts
+++ src/components/queries/topHeaders.ts
@@ -10,14 +10,14 @@ export const getTip = async () => {
     if (response.ok) {
       const data = await response.json();
       return {
-        currEpoch: (data[0]?.epoch_no),
-        absSlot: (data[0]?.abs_slot),
-        epochSlot: (data[0]?.epoch_slot),
-        blockNum: (data[0]?.block_no),
-        blockTime: (data[0]?.block_time),
-        } 
-    else {
-        throw new Error("Error fetching data");
+        currEpoch: data[0]?.epoch_no,
+        absSlot: data[0]?.abs_slot,
+        epochSlot: data[0]?.epoch_slot,
+        blockNum: data[0]?.block_no,
+        blockTime: data[0]?.block_time,
+      };
+    } else {
+      throw new Error("Error fetching data");
     }
   };
-
+  
