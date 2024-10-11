af4ebb6019d8e3946f5b2fdec329685ab0cc93b0
Manuel <econcotelli@proton.me>
2024-10-11 19:08:54 +0000

    mod

--- next.config.js
+++ next.config.js
@@ -23,6 +23,14 @@ const nextConfig = {
         source: '/api/:path*',
         destination: 'https://api.koios.rest/api/v1/:path*',
       },
+      {
+        source: '/api/current',  // Local endpoint to fetch current data
+        destination: 'http://136.243.208.16:12777/json/current.json',
+      },
+      {
+        source: '/api/next',  // Local endpoint to fetch next data
+        destination: 'http://136.243.208.16:12777/json/next.json',
+      },
     ];
   },
 };
