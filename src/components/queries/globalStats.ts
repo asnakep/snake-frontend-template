486194190a1f0801d830b6bf2b55a5ac54cbf20c
Manuel <econcotelli@proton.me>
2024-10-15 12:24:05 +0000

    modified

--- src/components/queries/globalStats.ts
+++ src/components/queries/globalStats.ts
@@ -1,15 +1,1 @@
-export const getLatestBlock = async () => {
-  const response = await fetch('/api/tip', {
-    method: 'GET',
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    return data[0]?.block_no;
-  } else {
-    throw new Error("Error fetching latest block");
-  }
-};
+import { getTip } from './queryTip';
