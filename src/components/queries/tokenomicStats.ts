d7c2b7053d32b02c6783eeee5417b4010ae438db
Manuel <econcotelli@proton.me>
2024-10-16 18:41:03 +0000

    modified

--- src/components/queries/tokenomicStats.ts
+++ src/components/queries/tokenomicStats.ts
@@ -4,9 +4,8 @@ import { getTip } from './queryTip';
 
 export const getTokenomicStats = async () => {
   try {
-    const tipData = await getTip(); // Use the current epoch from the tip
+    const tipData = await getTip();
 
-    // Fetch tokenomic data using the actual epoch number
     const response = await fetch(`/api/totals?_epoch_no=${tipData?.currEpoch}`, {
       method: 'GET',
       headers: {
@@ -17,18 +16,18 @@ export const getTokenomicStats = async () => {
 
     if (response.ok) {
       const data = await response.json();
-      const tokenomicData = data[0]; // Assuming the data returned is an array with a single object
+      const tokenomicData = data[0];
 
       return {
         epochNo: tokenomicData.epoch_no,
-        circulation: formatAda(tokenomicData.circulation), // Convert to ADA format
-        treasury: formatAda(tokenomicData.treasury), // Convert to ADA format
-        reward: formatAda(tokenomicData.reward), // Convert to ADA format
-        supply: formatAda(tokenomicData.supply), // Convert to ADA format
-        reserves: formatAda(tokenomicData.reserves) // Convert to ADA format
+        circulation: formatAda(tokenomicData.circulation),
+        treasury: formatAda(tokenomicData.treasury),
+        reward: formatAda(tokenomicData.reward),
+        supply: formatAda(tokenomicData.supply),
+        reserves: formatAda(tokenomicData.reserves)
       };
     } else {
-      console.error('Response status:', response.status); // Log the response status for debugging
+      console.error('Response status:', response.status);
       throw new Error("Error fetching tokenomic stats");
     }
   } catch (error) {
