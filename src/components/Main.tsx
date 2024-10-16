0381a91335fd29d52c00e795caa6691531dc9d57
Manuel <econcotelli@proton.me>
2024-10-16 11:56:20 +0000

    modified

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -54,13 +54,13 @@ const componentsConfig: ComponentConfig[] = [
   {
     id: "epochschedules",
     component: EpochSchedules,
-    position: { bottom: "180px", left: "480px", position: "fixed" },
+    position: { bottom: "210px", left: "480px", position: "fixed" },
     style: {},
   },
   {
     id: "poolrewards",
     component: PoolRewards,
-    position: { bottom: "90px", left: "480px", position: "fixed" },
+    position: { bottom: "100px", left: "480px", position: "fixed" },
     style: {},
   },
   {
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,10 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      spacing: {
-        '30': '7.5rem',  // Adds spacing equivalent to 120px
-        '40': '10rem',   // Adds spacing equivalent to 160px
-        '50': '12.5rem', // Adds spacing equivalent to 200px
-        // You can add more spacing values if needed
-      }
-    },
+    extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["black"],
+    themes: [ "black" ],
   },
 };
