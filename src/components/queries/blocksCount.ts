26100930ba735b1e57af5a0f3198ee62a960bcb0
Manuel <econcotelli@proton.me>
2024-10-16 18:29:27 +0000

    add

--- src/components/queries/blocksCount.ts
+++ src/components/queries/blocksCount.ts
@@ -1,9 +1,11 @@
+import koiosToken from '../variables/koiosToken'
+
 export const getBlocksCount = async (poolID: string, epochNo: number) => {
     const response = await fetch(`/api/pool_blocks?_pool_bech32=${poolID}&_epoch_no=${epochNo}`, {
       method: 'GET',
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyIjoic3Rha2UxdXlwdHFycHkyaG1semNrczJxOWhwdTgybTBsOWE5Z3N6OHB4cGV5ZGhqZnJwM2d2Zms1OG0iLCJleHAiOjE3MzIwMTkxNDcsInRpZXIiOjEsInByb2pJRCI6InNuYWtlcG9vbF9xdWVyaWVzIn0.080u6cvXuMxucyltftojjLNOEiPiul8_z2X3lmdYrzE`
+        "Authorization": `Bearer koiosToken`
       },
     });
   
