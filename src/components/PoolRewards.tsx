a6898d2f660c2727f9700117dc781f3d96046cca
Manuel <econcotelli@proton.me>
2024-10-17 18:41:40 +0000

    modified

--- src/components/PoolRewards.tsx
+++ src/components/PoolRewards.tsx
@@ -75,25 +75,25 @@ const PoolRewards = () => {
         </h3>
         <ul className="text-gray-300 space-y-2">
           <li className="flex justify-between text-xs gap-x-4">
-            <span style={{ marginRight: '178px' }}>
+            <span style={{ marginRight: '174px' }}>
               <i className="fas fa-calendar-day text-blue-400"></i>
               <strong>EPOCH</strong>
             </span>
-            <span className="text-blue-400 text-sm">{rewardsData?.epoch}</span>
+            <span className="custom-font">{rewardsData?.epoch}</span> {/* Apply custom font class */}
           </li>
           <li className="flex justify-between text-xs gap-x-4">
-            <span style={{ marginRight: '178px' }}>
+            <span style={{ marginRight: '174px' }}>
               <i className="fas fa-coins text-blue-400"></i>
               <strong>TOTAL</strong>
             </span>
-            <span className="text-blue-400 text-sm">{rewardsData?.rewards}</span>
+            <span className="custom-font">{rewardsData?.rewards}</span> {/* Apply custom font class */}
           </li>
           <li className="flex justify-between text-xs gap-x-4">
-            <span style={{ marginRight: '178px' }}>
+            <span style={{ marginRight: '174px' }}>
               <i className="fas fa-percentage text-blue-400"></i>
               <strong>EPOCH ROS</strong>
             </span>
-            <span className="text-blue-400 text-sm">{rewardsData?.ros}%</span>
+            <span className="custom-font">{rewardsData?.ros}%</span> {/* Apply custom font class */}
           </li>
         </ul>
       </div>
