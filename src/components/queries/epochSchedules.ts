c8da8a90cf72f8e77fda1b696ff32c7c3cc75f19
Manuel <econcotelli@proton.me>
2024-10-18 17:32:40 +0000

    modified:   src/components/queries/epochSchedules.ts

--- src/components/queries/epochSchedules.ts
+++ src/components/queries/epochSchedules.ts
@@ -1,15 +1,7 @@
 export const fetchEpochSchedules = async () => {
-  // Append a timestamp query parameter to the URL to prevent caching
-  const currentUrl = `http://136.243.208.16:12777/json/current.json?timestamp=${Date.now()}`;
+  const currentUrl = 'http://136.243.208.16:12777/json/current.json';
 
-  const currentResponse = await fetch(currentUrl, {
-    method: 'GET',
-    headers: {
-      'Cache-Control': 'no-cache', // Disable caching
-      'Pragma': 'no-cache',        // Additional header for backward compatibility
-      'Expires': '0',              // Ensure the response is not cached
-    },
-  });
+  const currentResponse = await fetch(currentUrl);
 
   if (!currentResponse.ok) {
     throw new Error('Failed to fetch current epoch data');
