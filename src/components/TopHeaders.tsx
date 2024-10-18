a960986793578cb12f72715b8ec98f1f147b7398
Manuel <econcotelli@proton.me>
2024-10-18 16:46:04 +0000

    modified:   src/components/TopHeaders.tsx

--- src/components/TopHeaders.tsx
+++ src/components/TopHeaders.tsx
@@ -1,45 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import Image from "next/image"; // If you want to use Next.js Image component instead
-import { getTip } from './queries/queryTip';
-
-interface TipData {
-  currEpoch: number;
-  epochSlot: number;
-  blockNum: number;
-}
+import React from 'react';
+import Image from "next/image"; // If you want to use Next.js Image component
 
 const TopHeaders: React.FC = () => {
-  const [tipData, setTipData] = useState<TipData | null>(null);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getTip();
-        setTipData(data);
-      } catch (err) {
-        setError("Failed to fetch top header data");
-      }
-    };
-
-    fetchData();
-    const interval = setInterval(fetchData, 20000); // Refresh every 20 seconds
-
-    return () => clearInterval(interval); // Cleanup the interval on component unmount
-  }, []);
-
-  if (error) {
-    return <div className="text-red-500">{error}</div>;
-  }
-
-  if (!tipData) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="flex flex-col p-4 bg-black-800 rounded-lg shadow-md">
       <div className="flex items-center space-x-4">
-        <img src="logo.svg" alt="SN₳KE Logo" className="h-14 w-auto" />
+        <Image
+          src="logo.svg" // Update the logo path as needed
+          alt="SN₳KE Logo"
+          className="h-14 w-auto"
+          width={56} // Adjust width as needed
+          height={56} // Adjust height as needed
+          priority
+        />
         <div className="text-white font-semibold text-2xl">
           Cardano Stakepool SN₳KE
         </div>
