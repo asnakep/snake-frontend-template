8e36fc14fd2f47dba6422ee86eb1bae43bcb6963
Manuel <econcotelli@proton.me>
2024-10-19 13:03:43 +0000

    modified:   src/components/CardanoStats.tsx

--- src/components/CardanoStats.tsx
+++ src/components/CardanoStats.tsx
@@ -54,74 +54,52 @@ const CardanoStats = () => {
       {error ? (
         <p className="text-red-500">Error: {error}</p>
       ) : (
-        <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-md p-4 mb-4">
+        <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-md p-6 mb-4">
           <div className="flex items-center justify-between mb-6">
             <div className="flex items-center">
               <img src="logo-cardano.svg" alt="Cardano Logo" className="h-12 w-12" />
               <h3 className="text-white text-lg font-bold ml-4">CARDANO</h3>
             </div>
-            {loading ? (
-              <div className="flex items-center justify-center w-60 h-7">
-                <FontAwesomeIcon icon={faSpinner} spin className="text-white" />
-              </div>
-            ) : (
-              <div className="relative w-60 bg-black-800 h-7 overflow-hidden rounded-sm ml-2">
-                <div
-                  className="absolute top-0 left-0 bg-blue-800 h-full rounded-sm ml-16"
-                  style={{ width: typeof epochProgressPercent === 'string' && epochProgressPercent !== "N/A" ? `${epochProgressPercent}%` : "0%" }}
-                ></div>
-                <div className="absolute inset-0 flex justify-center items-center text-white font-semibold text-sm">
-                  <span className="px-6">Epoch {formatNumber(epoch) || "N/A"}</span>
+            <div className="relative w-60 bg-black-800 h-7 overflow-hidden rounded-sm ml-2">
+              {loading ? (
+                <div className="absolute inset-0 flex justify-center items-center text-white">
+                  <FontAwesomeIcon icon={faSpinner} spin className="text-white" />
                 </div>
-                <div className="absolute inset-0 flex justify-end items-center pr-2 text-white font-semibold text-sm">
-                  <span className="px-6">{`${epochProgressPercent}%`}</span>
-                </div>
-              </div>
-            )}
+              ) : (
+                <>
+                  <div
+                    className="absolute top-0 left-10 bg-blue-800 h-full rounded-sm"
+                    style={{ width: `${epochProgressPercent}%` }}
+                  ></div>
+                  <div className="absolute inset-0 flex justify-center items-center text-white font-semibold text-sm">
+                    <span className="px-6">Epoch {formatNumber(epoch) || "N/A"}</span>
+                  </div>
+                  <div className="absolute inset-0 flex justify-end items-center pr-2 text-white font-semibold text-sm">
+                    <span className="px-6">{`${epochProgressPercent}%`}</span>
+                  </div>
+                </>
+              )}
+            </div>
           </div>
 
           <div className="mt-2">
             <ul className="text-gray-300 space-y-2">
-              <li className="flex justify-between text-xs">
-                <span className="mr-60">
-                  <i className="fas fa-calendar-alt text-blue-400"></i> <strong>EPOCH</strong>
-                </span>
-                <span className="text-blue-400 text-sm custom-font">
-                  {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : formatNumber(epoch) || "N/A"}
-                </span>
-              </li>
-              <li className="flex justify-between text-xs">
-                <span className="mr-60">
-                  <i className="fas fa-clock text-blue-400"></i> <strong>SLOT</strong>
-                </span>
-                <span className="text-blue-400 text-sm custom-font">
-                  {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : formatNumber(epochSlot) || "N/A"}
-                </span>
-              </li>
-              <li className="flex justify-between text-xs">
-                <span className="mr-60">
-                  <i className="fas fa-cube text-blue-400"></i> <strong>BLOCK</strong>
-                </span>
-                <span className="text-blue-400 text-sm custom-font">
-                  {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : formatNumber(tipData?.blockNum) || "N/A"}
-                </span>
-              </li>
-              <li className="flex justify-between text-xs">
-                <span className="mr-60">
-                  <i className="fas fa-list text-blue-400"></i> <strong>TXS COUNT</strong>
-                </span>
-                <span className="text-blue-400 text-sm custom-font">
-                  {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : formatNumber(cardanoStats?.txCount) || "N/A"}
-                </span>
-              </li>
-              <li className="flex justify-between text-xs">
-                <span className="mr-60 mb-4">
-                  <i className="fas fa-cube text-blue-400"></i> <strong>BLOCK COUNT</strong>
-                </span>
-                <span className="text-blue-400 text-sm custom-font">
-                  {loading ? <FontAwesomeIcon icon={faSpinner} spin /> : formatNumber(cardanoStats?.blkCount) || "N/A"}
-                </span>
-              </li>
+              {[
+                { label: 'EPOCH', value: epoch },
+                { label: 'SLOT', value: epochSlot },
+                { label: 'BLOCK', value: tipData?.blockNum },
+                { label: 'TXS COUNT', value: cardanoStats?.txCount },
+                { label: 'BLOCK COUNT', value: cardanoStats?.blkCount }
+              ].map(({ label, value }, index) => (
+                <li key={index} className="flex justify-between text-xs">
+                  <span className="mr-60">
+                    <i className={`fas fa-${label === 'EPOCH' ? 'calendar-alt' : label === 'SLOT' ? 'clock' : 'cube'} text-blue-400`}></i> <strong>{label}</strong>
+                  </span>
+                  <span className="text-blue-400 text-sm custom-font">
+                    {value !== undefined && value !== null ? formatNumber(value) : <FontAwesomeIcon icon={faSpinner} spin />}
+                  </span>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
