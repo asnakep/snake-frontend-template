157252cfdccfe823ac9dbe34699d71aaf9fbca5d
Manuel <econcotelli@proton.me>
2024-10-19 16:20:55 +0000

    modified:   public/loading-spinner.svg 	modified:   src/components/LoadingPopup.tsx

--- src/components/LoadingPopup.tsx
+++ src/components/LoadingPopup.tsx
@@ -1,17 +1,12 @@
-// LoadingPopup.tsx
+import Image from "next/image";
 import React from 'react';
 
-const LoadingPopup: React.FC<{ message?: string }> = ({ message = "Loading..." }) => {
+const LoadingPopup: React.FC = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-70 z-50">
-      <div className="bg-white p-4 rounded-sm shadow-lg flex items-center">
-        <div className="animate-spin mr-2">
-          <svg className="w-8 h-8 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <circle className="opacity-25" cx="12" cy="12" r="10" strokeWidth="4" />
-            <path className="opacity-75" fill="blue" d="M4 12a8 8 0 0014.488 3.36l1.428-1.428A10 10 0 10 4 12z" />
-          </svg>
-        </div>
-        <p className="text-lg text-gray-800">{message}</p>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-80 z-50">
+      <div className="flex items-center">
+        {/* Use next/image for optimization */}
+        <Image src="/loading-spinner.svg" alt="Loading..." width={200} height={200} /> {/* Adjust size as needed */}
       </div>
     </div>
   );
