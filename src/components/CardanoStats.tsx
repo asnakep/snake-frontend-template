e46a13f68b7f230a5331cfc4aa79242ef5442298
Manuel <econcotelli@proton.me>
2024-10-19 13:34:01 +0000

    modified:   src/components/AdaPrice.tsx 	modified:   src/components/CardanoStats.tsx

--- src/components/CardanoStats.tsx
+++ src/components/CardanoStats.tsx
@@ -1,9 +1,25 @@
 import { useState, useEffect } from 'react';
 import { getCardanoStats } from './queries/cardanoStats';
 import { getTip } from './queries/queryTip';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+// Loading Popup Component
+const LoadingPopup: React.FC<{ message?: string }> = ({ message = "Loading..." }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-70 z-50">
+      <div className="bg-white p-4 rounded-lg shadow-lg flex items-center">
+        <div className="animate-spin mr-2">
+          <svg className="w-8 h-8 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <circle className="opacity-25" cx="12" cy="12" r="10" strokeWidth="4" />
+            <path className="opacity-75" fill="blue" d="M4 12a8 8 0 0014.488 3.36l1.428-1.428A10 10 0 10 4 12z" />
+          </svg>
+        </div>
+        <p className="text-lg text-gray-800">{message}</p>
+      </div>
+    </div>
+  );
+};
 
 const CardanoStats = () => {
   const [cardanoStats, setCardanoStats] = useState<any>(null);
@@ -31,9 +47,9 @@ const CardanoStats = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      setLoading(true);
+      setLoading(true); // Start loading
       await Promise.all([fetchTipData(), fetchCardanoStats()]);
-      setLoading(false);
+      setLoading(false); // End loading
     };
 
     fetchData();
@@ -55,7 +71,7 @@ const CardanoStats = () => {
       {error ? (
         <p className="text-red-500">Error: {error}</p>
       ) : (
-        <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-md p-6 mb-4">
+        <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-md p-6 mb-4 relative">
           <div className="flex items-center justify-between mb-6">
             <div className="flex items-center">
               <img src="logo-cardano.svg" alt="Cardano Logo" className="h-12 w-12" />
@@ -103,6 +119,8 @@ const CardanoStats = () => {
               ))}
             </ul>
           </div>
+
+          {loading && <LoadingPopup />} {/* Show LoadingPopup when loading */}
         </div>
       )}
     </div>
