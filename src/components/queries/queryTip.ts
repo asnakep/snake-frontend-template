c5bd331421d19a2d1863120c0619069c7d38432d
Manuel <econcotelli@proton.me>
2024-10-23 19:13:54 +0000

    changes

--- src/components/queries/queryTip.ts
+++ src/components/queries/queryTip.ts
@@ -14,10 +14,8 @@ export const getTip = async () => {
       const data = await response.json();
       return {
         currEpoch: data[0]?.epoch_no,
-        absSlot: data[0]?.abs_slot,
         epochSlot: data[0]?.epoch_slot,
         blockNum: data[0]?.block_no,
-        blockTime: new Date(data[0]?.block_time * 1000).toLocaleString(),
       };
     } else {
       throw new Error("Error fetching data");
