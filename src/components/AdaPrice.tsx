94630df906213a1022dc66ba9e7c8e7e701d2867
Manuel <econcotelli@proton.me>
2024-10-30 17:49:57 +0000

    modified:   src/components/AdaPrice.tsx 	modified:   src/components/BlocksProduction.tsx 	modified:   src/components/CardanoStats.tsx 	modified:   src/components/LoadingPopup.tsx 	modified:   src/components/PoolRewards.tsx

--- src/components/AdaPrice.tsx
+++ src/components/AdaPrice.tsx
@@ -49,7 +49,7 @@ const AdaPricePanel = () => {
   };
 
   return (
-    <div className="max-w-4xl w-full bg-gray-800 rounded-lg shadow-md p-4 mb-1">
+    <div className="max-w-4xl w-full bg-gray-900 rounded-lg p-4 mb-1">
       {error ? (
         <p className="text-red-500">Error: {error}</p>
       ) : (
--- src/components/LoadingPopup.tsx
+++ src/components/LoadingPopup.tsx
@@ -6,7 +6,7 @@ const LoadingPopup: React.FC = () => {
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-80 z-50">
       <div className="flex items-center">
         {/* Use next/image for optimization */}
-        <Image src="/loading-spinner.svg" alt="Loading..." width={100} height={100} /> {/* Adjust size as needed */}
+        <Image src="/loading-spinner.svg" alt="Loading..." width={60} height={60} /> {/* Adjust size as needed */}
       </div>
     </div>
   );
