9088a8a345bdcab3d7fb284aa3118b1e6ce1feda
Manuel <econcotelli@proton.me>
2024-10-31 12:31:44 +0000

    modified:   src/components/LoadingPopup.tsx

--- src/components/LoadingPopup.tsx
+++ src/components/LoadingPopup.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const LoadingPopup: React.FC = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-80 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-base-100 bg-opacity-80 z-50">
       <div className="flex items-center">
         {/* Use next/image for optimization */}
         <Image src="/loading-spinner.svg" alt="Loading..." width={60} height={60} /> {/* Adjust size as needed */}
